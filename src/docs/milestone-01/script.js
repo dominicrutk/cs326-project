967700290c7c22513d1ad11f11a4074df1fe2c08
thuphan1312 <tnphan@umass.edu>
2024-03-25 14:36:35 -0400

    Adding JS component for Sliding Header Image
    In this commit, I added a JS carousel to change the header image every 5 seconds on "mouseenter" event.

--- src/docs/milestone-01/script.js
+++ src/docs/milestone-01/script.js
@@ -1,1 +1,19 @@
-"use strict";
+function headerImageSlider() {
+    let i = 0
+    let slider = document.getElementById('header-slide')
+    const header_interval = setInterval(function() {
+        if (i >= 0 && i < 2) {
+            i++
+            slider.src = "image/header" + i + ".jpg"
+        }
+        else if (i >= 2) {
+            i = 0
+            slider.src = "image/header" + i + ".jpg"
+        }
+    }, 5000)
+}
+
+
+// Add Event Listeners
+document.getElementById('header-slide').addEventListener('mouseenter', headerImageSlider)
+
